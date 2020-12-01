0247ddeee3ecbb4861523e2ee07beb1bcd568dd5
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2020-12-01 20:41:26 +0100

    Make log function variadic

--- libovvcutils/ovvcutils.c
+++ libovvcutils/ovvcutils.c
@@ -1,14 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
 #include "ovvcutils.h"
-#include "stdio.h"
 
 static int ov_log_level = 1;
+static const char* vvctype = "VVCDec";
 
-/*TODO proper structures initialisation etc. */
 void
-ov_log(void* ctx, int log_level, const char* log_content)
+ov_log(void* ctx, int log_level, const char* log_content, ... )
 {
+    va_list args;
+
+    va_start(args, log_content);
+
     if (log_level > ov_log_level){
-        printf("[NULL @ Ox%.16lx] : %s", (long unsigned)ctx, log_content);
+        const char *type = "NULL";
+        if (ctx != NULL){
+            type = vvctype;
+        }
+        printf("[%s @ Ox%.16lx] : ", type, (long unsigned)ctx);
+        vprintf(log_content, args);
     }
+
+    va_end(args);
 }
 
--- libovvcutils/ovvcutils.h
+++ libovvcutils/ovvcutils.h
@@ -1,7 +1,7 @@
 #ifndef OVVCUTILS_H
 #define OVVCUTILS_H
 
-void ov_log(void* ctx, int log_level, const char* log_content);
+void ov_log(void* ctx, int log_level, const char* log_content, ... );
 
 #endif
 
