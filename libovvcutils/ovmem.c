9aafe61ffad16eeed302a24ed90082414c2a18a7
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2020-12-07 13:24:09 +0100

    Add ovmem.c and ovmem.h
    Those files implements some wrappers around malloc/free functions
    so we can keep later modification such as memory alignement
    in one place only when we will want to get aligned memory.

---
+++ libovvcutils/ovmem.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include "ovmem.h"
+
+/* Wrappers around memory related function this will
+   be useful if we plan later to adapt our allocations to
+   to use aligned memory etc.  without changing it in
+   every place we called malloc etc.*/
+
+void *
+ov_malloc(size_t alloc_size)
+{
+    void *ptr = malloc(alloc_size);
+
+    return ptr;
+}
+
+void *
+ov_mallocz(size_t alloc_size)
+{
+    void *ptr = malloc(alloc_size);
+
+    if (ptr != NULL) {
+        memset(ptr, 0, alloc_size);
+    }
+
+    return ptr;
+}
+
+void
+ov_free(void *ptr)
+{
+    free(ptr);
+}
+
+/* This is inspired from FFmpeg av_freep function
+ * so we can free and set a pointer to NULL
+ */
+void
+ov_freep(void *ptr_ref)
+{
+     void *ptr_cpy;
+    /* ptr_cpy = *ptr_ref = ptr*/
+    memcpy(&ptr_cpy, ptr_ref, sizeof(ptr_cpy));
+
+     /* *ptr_ref = &{NULL}  --> ptr = NULL */
+
+    memcpy(ptr_ref, &(void *){NULL}, sizeof(ptr_cpy));
+
+    ov_free(ptr_cpy);
+}
+
+
---
+++ libovvcutils/ovmem.h
@@ -0,0 +1,12 @@
+#ifndef OVMEM_H
+#define OVMEM_H
+#include <stddef.h>
+
+void *ov_mallocz(size_t alloc_size);
+
+void ov_free(void *ptr);
+
+void ov_freep(void *ptr_ref);
+
+#endif
+
