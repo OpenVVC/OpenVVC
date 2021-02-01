bf7a090d60f42a845d6498d4c88f339fb82f2430
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-02-01 11:44:15 +0100

    Fix Picture Header reading

--- libovvcdec/nvcl_nal_ph.c
+++ libovvcdec/nvcl_nal_ph.c
@@ -95,20 +95,36 @@ nvcl_ph_read(OVNVCLReader *const rdr, OVPH *const ph,
     }
 
     ph->ph_pic_parameter_set_id = nvcl_read_u_expgolomb(rdr);
-    ph->ph_pic_order_cnt_lsb    = nvcl_read_bits(rdr,);
+
+    if (ph->ph_pic_parameter_set_id < OV_MAX_NUM_PPS) {
+        uint8_t pps_id = ph->ph_pic_parameter_set_id & 0xF;
+        pps = nvcl_ctx->pps_list[pps_id];
+        if (pps) {
+            /* We suppose sps_id already checked by sps reader */
+            uint8_t sps_id = pps->pps_seq_parameter_set_id & 0xF;
+            sps = nvcl_ctx->sps_list[sps_id];
+        }
+        if (!pps || !sps) {
+            ov_log(NULL, 3, "SPS or PPS missing when trying to decode PH\n");
+            return OV_INVALID_DATA;
+        }
+    }
+
+    ph->ph_pic_order_cnt_lsb = nvcl_read_bits(rdr, sps->sps_log2_max_pic_order_cnt_lsb_minus4 + 4);
 
     if (ph->ph_gdr_pic_flag) {
         ph->ph_recovery_poc_cnt = nvcl_read_u_expgolomb(rdr);
     }
 
-    for (i = 0; i < NumExtraPhBits; i++) {
+    /* FIXME this suppose to count 1 in sps_ph_extra_bits table */
+    for (i = 0; i < sps->sps_num_extra_ph_bytes; i++) {
         ph->ph_extra_bit[i] = nvcl_read_bits(rdr, 1);
     }
 
-    if (sps->sps_poc_msb_cycle_flag) { 
+    if (sps->sps_poc_msb_cycle_flag) {
         ph->ph_poc_msb_cycle_present_flag = nvcl_read_flag(rdr);
         if (ph->ph_poc_msb_cycle_present_flag) {
-            ph->ph_poc_msb_cycle_val = nvcl_read_bits(rdr,);
+            ph->ph_poc_msb_cycle_val = nvcl_read_bits(rdr, sps->sps_poc_msb_cycle_len_minus1 + 1);
         }
     }
 
@@ -180,7 +196,11 @@ nvcl_ph_read(OVNVCLReader *const rdr, OVPH *const ph,
     }
 
     if (pps->pps_rpl_info_in_ph_flag) {
+         struct OVHRPL *const hrpl = &ph->hrpl;
+         nvcl_read_header_ref_pic_lists(rdr, hrpl, sps, pps);
+        #if 0
         ref_pic_lists();
+        #endif
     }
 
     if (sps->sps_partition_constraints_override_enabled_flag) {
@@ -236,9 +256,12 @@ nvcl_ph_read(OVNVCLReader *const rdr, OVPH *const ph,
         if (sps->sps_temporal_mvp_enabled_flag) {
             ph->ph_temporal_mvp_enabled_flag = nvcl_read_flag(rdr);
             if (ph->ph_temporal_mvp_enabled_flag && pps->pps_rpl_info_in_ph_flag) {
-                if (num_ref_entries[1][RplsIdx[1]] > 0) {
+                /* FIXME compute num_ref_entries */
+                int num_ref_entries1 = 0; /* num_ref_entries[1][RplsIdx[1]] */
+                if (num_ref_entries1 > 0) {
+                    int num_ref_entries0 = 0; /* num_ref_entries[0][RplsIdx[0]] */
                     ph->ph_collocated_from_l0_flag = nvcl_read_flag(rdr);
-                    if ((ph->ph_collocated_from_l0_flag && num_ref_entries[0][RplsIdx[0]] > 1) || (!ph->ph_collocated_from_l0_flag && num_ref_entries[1][RplsIdx[1]] > 1)) {
+                    if ((ph->ph_collocated_from_l0_flag && num_ref_entries0 > 1) || (!ph->ph_collocated_from_l0_flag && num_ref_entries1 > 1)) {
                         ph->ph_collocated_ref_idx = nvcl_read_u_expgolomb(rdr);
                     }
                 }
@@ -249,13 +272,23 @@ nvcl_ph_read(OVNVCLReader *const rdr, OVPH *const ph,
             ph->ph_mmvd_fullpel_only_flag = nvcl_read_flag(rdr);
         }
 
-        int presenceFlag = 0;
-        if (!pps->pps_rpl_info_in_ph_flag) { /* This condition is intentionally not merged into the next,
-                                          to avoid possible interpretation of RplsIdx[i] not having a specified value. */
-            presenceFlag = 1;
-        } else if (num_ref_entries[1][RplsIdx[1]] > 0) {
-            presenceFlag = 1;
-            if (presenceFlag) {
+        /* FIXME Num ref entries can be undefined if rpl info are in PH */
+        int num_ref_entries1 = 0;
+        if (!pps->pps_rpl_info_in_ph_flag) {
+
+            ph->ph_mvd_l1_zero_flag = nvcl_read_flag(rdr);
+            if (sps->sps_bdof_control_present_in_ph_flag) {
+                ph->ph_bdof_disabled_flag = nvcl_read_flag(rdr);
+            }
+
+            if (sps->sps_dmvr_control_present_in_ph_flag) {
+                ph->ph_dmvr_disabled_flag = nvcl_read_flag(rdr);
+            }
+
+        } else if (num_ref_entries1 > 0){
+
+            /* FIXME compute num_ref_entries */
+            if (num_ref_entries1 > 0) {
                 ph->ph_mvd_l1_zero_flag = nvcl_read_flag(rdr);
                 if (sps->sps_bdof_control_present_in_ph_flag) {
                     ph->ph_bdof_disabled_flag = nvcl_read_flag(rdr);
@@ -272,7 +305,9 @@ nvcl_ph_read(OVNVCLReader *const rdr, OVPH *const ph,
         }
 
         if ((pps->pps_weighted_pred_flag || pps->pps_weighted_bipred_flag) && pps->pps_wp_info_in_ph_flag) {
+            #if 0
             pred_weight_table()
+            #endif
         }
     }
 
@@ -289,7 +324,6 @@ nvcl_ph_read(OVNVCLReader *const rdr, OVPH *const ph,
         if (sps->sps_chroma_format_idc != 0) {
             ph->ph_sao_chroma_enabled_flag = nvcl_read_flag(rdr);
         }
-
     }
 
     if (pps->pps_dbf_info_in_ph_flag) {
