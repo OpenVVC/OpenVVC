3bccc12f1e44be0ae325e9cb285e6d32f8c87b8e
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-02-05 14:47:53 +0100

    Move dec structures definitions to header

--- libovvcdec/ovvcdec.c
+++ libovvcdec/ovvcdec.c
@@ -8,112 +8,15 @@
 
 #include "nvcl.h"
 #include "ovvcdec.h"
+#include "dec_structures.h"
 
 static const char *const decname = "Open VVC Decoder";
 
 struct OVVCSubDec;
 
-struct OVSPSInfo{
-    struct {
-      uint8_t lfnst;
-      uint8_t mts;
-      uint8_t mrl;
-      uint8_t mip;
-      uint8_t isp;
-      uint8_t cclm;
-    } tool_flags;
-
-    struct {
-        uint8_t chroma_format;
-        uint16_t pic_w;
-        uint16_t pic_h;
-        /*TODO conformance window */
-        
-    } pic_info;
-
-    struct {
-      uint8_t log2_ctb_s;
-      uint8_t log2_min_cb_s;
-      
-    } part_info[2];
-
-    #if 0
-    struct {
-    } DPBInfo;
-
-    /* Chroma QP tables */
-    struct {
-    } qp_tables[3];
-    #endif
-
-    uint8_t bitdepth;
-};
-
-struct OVPPSInfo{
-
-    /* Sub Picture / Tiles overrides */
-    struct {
-        uint16_t pic_w;
-        uint16_t pic_h;
-        uint8_t log2_ctb_s;
-        /*TODO conformance and scaling window */
-        
-    } pic_info;
-
-    struct {
-      uint8_t pic_init_qp;
-      /* TODO use offset tables */
-      uint8_t qp_offset_c[3];
-      struct {
-        int8_t qp_offset[16];
-      } offset_list[3];
-    } pic_qp_info;
-
-};
 
 
 
-/* Main decoder structure */
-struct OVVCDec
-{
-    const char *name;
-
-    /* NAL Units to be decoded
-     * Corresponding to a Picture Unit
-     */
-    OVPictureUnit *nalu_list;
-
-    /* Paramters sets context */
-    OVNVCLCtx nvcl_ctx;
-
-    struct OVPS{
-        /* Pointers to active parameter sets */
-        const OVSPS *sps;
-        const OVPPS *pps;
-        const OVPH *ph;
-        const OVSH *sh;
-
-        /* Human readable information from active parameter sets */
-        struct OVSPSInfo sps_info;
-        struct OVPPSInfo pps_info;
-        #if 0
-        struct OVPHInfo ph_info;
-        #endif
-    } active_params;
-
-
-    /* List of Sub Decoders
-     * Contains context for Tile / Slice / Picture / SubPicture
-     * decoding
-     */
-    struct OVSubDec *subdec_list;
-
-    /* Informations on decoder behaviour transmitted by user
-     */
-    struct {
-        int opt1;
-    }options;
-};
 
 /* Actions on unsupported NAL Unit types */
 static int nalu_type_unsupported(enum OVNALUType nalu_type);
