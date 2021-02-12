8208837bdef565471ebaa332a370b5b0355dcf3a
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-02-12 11:06:19 +0100

    Harmonize ctudec namings in rcn.c

--- libovvc/rcn.c
+++ libovvc/rcn.c
@@ -224,7 +224,7 @@ vvc_intra_chroma_angular(const uint16_t *const src, uint16_t *const dst,
     }
 }
 
-void vvc_intra_pred_chroma(const OVCTUDec *const lc_ctx,
+void vvc_intra_pred_chroma(const OVCTUDec *const ctudec,
                            uint16_t *const dst_cb, uint16_t *const dst_cr,
                            ptrdiff_t dst_stride,
                            uint8_t intra_mode, int x0, int y0,
@@ -235,17 +235,17 @@ void vvc_intra_pred_chroma(const OVCTUDec *const lc_ctx,
     uint16_t ref_left [(128<<1) + 128]/*={0}*/;
     uint16_t *ref1 = ref_above;
     uint16_t *ref2 = ref_left;
-    uint8_t neighbour= lc_ctx->ctu_ngh_flags;
+    uint8_t neighbour= ctudec->ctu_ngh_flags;
     uint8_t got_left_ctu = neighbour & VVC_CTU_LEFT_FLAG;
     uint8_t got_top_ctu  = neighbour & VVC_CTU_UP_FLAG;
-    // FIXED? : const uint16_t *const src_cb = &lc_ctx->ctu_data_cb[VVC_CTB_OFFSET];
-    // FIXED? : const uint16_t *const src_cr = &lc_ctx->ctu_data_cr[VVC_CTB_OFFSET];
-    const uint16_t *const src_cb = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_cb[VVC_CTB_OFFSET];
-    const uint16_t *const src_cr = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_cr[VVC_CTB_OFFSET];
-    // FIXED? : uint64_t left_col_map = lc_ctx->progress_map_c.cols[x0 >> 1];
-    // FIXED? : uint64_t top_row_map  = lc_ctx->progress_map_c.rows[y0 >> 1];
-    uint64_t left_col_map = lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 1];
-    uint64_t top_row_map  = lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 1];
+    // FIXED? : const uint16_t *const src_cb = &ctudec->ctu_data_cb[VVC_CTB_OFFSET];
+    // FIXED? : const uint16_t *const src_cr = &ctudec->ctu_data_cr[VVC_CTB_OFFSET];
+    const uint16_t *const src_cb = &ctudec->rcn_ctx.rcn_ctu_buff.data_cb[VVC_CTB_OFFSET];
+    const uint16_t *const src_cr = &ctudec->rcn_ctx.rcn_ctu_buff.data_cr[VVC_CTB_OFFSET];
+    // FIXED? : uint64_t left_col_map = ctudec->progress_map_c.cols[x0 >> 1];
+    // FIXED? : uint64_t top_row_map  = ctudec->progress_map_c.rows[y0 >> 1];
+    uint64_t left_col_map = ctudec->rcn_ctx.progress_field.vfield[x0 >> 1];
+    uint64_t top_row_map  = ctudec->rcn_ctx.progress_field.hfield[y0 >> 1];
 
 
     switch (intra_mode) {
@@ -325,20 +325,20 @@ void vvc_intra_pred_chroma(const OVCTUDec *const lc_ctx,
     }
     case VVC_LM_CHROMA:
     {
-        // FIXED? : const uint16_t  *const src_luma = &lc_ctx->ctu_data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
-        const uint16_t  *const src_luma = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
+        // FIXED? : const uint16_t  *const src_luma = &ctudec->ctu_data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
+        const uint16_t  *const src_luma = &ctudec->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
 
-        // lc_ctx->cclm_func(src_luma, dst_cb, dst_cr, log2_pb_width, log2_pb_height,
+        // ctudec->cclm_func(src_luma, dst_cb, dst_cr, log2_pb_width, log2_pb_height,
         //                   y0, got_top_ctu || y0, got_left_ctu || x0);
 
         break;
     }
     case VVC_MDLM_LEFT:
     {
-      // FIXED? : const uint16_t  *const src_luma = &lc_ctx->ctu_data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
-      const uint16_t  *const src_luma = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
+      // FIXED? : const uint16_t  *const src_luma = &ctudec->ctu_data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
+      const uint16_t  *const src_luma = &ctudec->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
 
-        // FIXED? : lc_ctx->left_mdlm(src_luma, dst_cb, dst_cr,
+        // FIXED? : ctudec->left_mdlm(src_luma, dst_cb, dst_cr,
         //                   left_col_map, log2_pb_width, log2_pb_height,
         //                   x0, y0, x0 || got_left_ctu, y0 || got_top_ctu);
 
@@ -346,10 +346,10 @@ void vvc_intra_pred_chroma(const OVCTUDec *const lc_ctx,
     }
     case VVC_MDLM_TOP:
     {
-      // FIXED? : const uint16_t  *const src_luma = &lc_ctx->ctu_data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
-        const uint16_t  *const src_luma = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
+      // FIXED? : const uint16_t  *const src_luma = &ctudec->ctu_data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
+        const uint16_t  *const src_luma = &ctudec->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET+(x0<<1)+((y0<<1)*VVC_CTB_STRIDE)];
 
-        // FIXED? : lc_ctx->top_mdlm(src_luma, dst_cb, dst_cr,
+        // FIXED? : ctudec->top_mdlm(src_luma, dst_cb, dst_cr,
         //                  top_row_map, log2_pb_width, log2_pb_height,
         //                  x0, y0, x0 || got_left_ctu, y0 || got_top_ctu);
 
@@ -370,7 +370,7 @@ void vvc_intra_pred_chroma(const OVCTUDec *const lc_ctx,
     }
 }
 
-void vvc_intra_pred(const OVCTUDec *const lc_ctx,
+void vvc_intra_pred(const OVCTUDec *const ctudec,
                     uint16_t *const src,
                     ptrdiff_t dst_stride,
                     uint8_t intra_mode, int x0, int y0,
@@ -384,23 +384,23 @@ void vvc_intra_pred(const OVCTUDec *const lc_ctx,
     uint16_t *ref1 = ref_above + (1 << log2_pb_height);
     uint16_t *ref2 = ref_left + (1 << log2_pb_width);
     // FIXED?: fill_ref_left_0(src,dst_stride,ref2,
-    //                 lc_ctx->progress_map.cols[x0 >> 2],
-    //         lc_ctx->progress_map.rows[y0 >> 2],
+    //                 ctudec->progress_map.cols[x0 >> 2],
+    //         ctudec->progress_map.rows[y0 >> 2],
     //         x0, y0, log2_pb_width, log2_pb_height,0);
 
             fill_ref_left_0(src,dst_stride,ref2,
-                            lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
-                    lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
+                            ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
+                    ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
                     x0, y0, log2_pb_width, log2_pb_height,0);
 
     // FIXED?: fill_ref_above_0(src, dst_stride, ref1,
-            //          lc_ctx->progress_map.rows[y0 >> 2],
-            // lc_ctx->progress_map.cols[x0 >> 2],
+            //          ctudec->progress_map.rows[y0 >> 2],
+            // ctudec->progress_map.cols[x0 >> 2],
             // x0, y0, log2_pb_width, log2_pb_height,0);
 
             fill_ref_above_0(src, dst_stride, ref1,
-                             lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
-                    lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
+                             ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
+                    ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
                     x0, y0, log2_pb_width, log2_pb_height,0);
     switch (intra_mode) {
     case VVC_PLANAR://PLANAR
@@ -699,7 +699,7 @@ void vvc_intra_pred(const OVCTUDec *const lc_ctx,
     }
 }
 
-void vvc_intra_pred_isp(const OVCTUDec *const lc_ctx,
+void vvc_intra_pred_isp(const OVCTUDec *const ctudec,
                         uint16_t *const src,
                         ptrdiff_t dst_stride,
                         uint8_t intra_mode,
@@ -715,20 +715,20 @@ void vvc_intra_pred_isp(const OVCTUDec *const lc_ctx,
     uint16_t *ref2 = ref_left + (1 << log2_pb_width);
 
     // FIXED? :fill_ref_left_0(src, dst_stride, ref2,
-    //                 lc_ctx->progress_map.cols[(x0 >> 2) + !!(offset_x % 4)],
-    //                 lc_ctx->progress_map.rows[((y0 ) >> 2) + !!(y0 % 4)],
+    //                 ctudec->progress_map.cols[(x0 >> 2) + !!(offset_x % 4)],
+    //                 ctudec->progress_map.rows[((y0 ) >> 2) + !!(y0 % 4)],
     //                 x0, y0 - offset_y, log2_cu_width, log2_cu_height, offset_y);
     fill_ref_left_0(src, dst_stride, ref2,
-                    lc_ctx->rcn_ctx.progress_field.vfield[(x0 >> 2) + !!(offset_x % 4)],
-                    lc_ctx->rcn_ctx.progress_field.hfield[((y0 ) >> 2) + !!(y0 % 4)],
+                    ctudec->rcn_ctx.progress_field.vfield[(x0 >> 2) + !!(offset_x % 4)],
+                    ctudec->rcn_ctx.progress_field.hfield[((y0 ) >> 2) + !!(y0 % 4)],
                     x0, y0 - offset_y, log2_cu_width, log2_cu_height, offset_y);
     //  FIXED? :fill_ref_above_0(src, dst_stride, ref1,
-    //                  lc_ctx->progress_map.rows[(y0 >> 2) + !!(offset_y % 4)],
-    //                  lc_ctx->progress_map.cols[((x0 ) >> 2) + !!(x0 % 4)],
+    //                  ctudec->progress_map.rows[(y0 >> 2) + !!(offset_y % 4)],
+    //                  ctudec->progress_map.cols[((x0 ) >> 2) + !!(x0 % 4)],
     //                  x0 - offset_x, y0, log2_cu_width, log2_cu_height, offset_x);
    fill_ref_above_0(src, dst_stride, ref1,
-                    lc_ctx->rcn_ctx.progress_field.hfield[(y0 >> 2) + !!(offset_y % 4)],
-                    lc_ctx->rcn_ctx.progress_field.vfield[((x0 ) >> 2) + !!(x0 % 4)],
+                    ctudec->rcn_ctx.progress_field.hfield[(y0 >> 2) + !!(offset_y % 4)],
+                    ctudec->rcn_ctx.progress_field.vfield[((x0 ) >> 2) + !!(x0 % 4)],
                     x0 - offset_x, y0, log2_cu_width, log2_cu_height, offset_x);
     ref1+= offset_x;
     ref2+= offset_y;
@@ -911,7 +911,7 @@ void vvc_intra_pred_isp(const OVCTUDec *const lc_ctx,
     }
 }
 
-void vvc_intra_pred_multi_ref( const OVCTUDec *const lc_ctx,
+void vvc_intra_pred_multi_ref( const OVCTUDec *const ctudec,
                                uint16_t *const src,
                                ptrdiff_t dst_stride,
                                uint8_t intra_mode, int x0, int y0,
@@ -924,23 +924,23 @@ void vvc_intra_pred_multi_ref( const OVCTUDec *const lc_ctx,
     uint16_t *ref1 = ref_above + (1 << log2_pb_height);
     uint16_t *ref2 = ref_left  + (1 << log2_pb_width);
 // FIXED? fill_ref_left_0_mref(src, dst_stride, ref2,
-//                      lc_ctx->progress_map.cols[x0 >> 2],
-//         lc_ctx->progress_map.rows[y0 >> 2],
+//                      ctudec->progress_map.cols[x0 >> 2],
+//         ctudec->progress_map.rows[y0 >> 2],
 //         multi_ref_idx, x0, y0,
 //         log2_pb_width, log2_pb_height);
 fill_ref_left_0_mref(src, dst_stride, ref2,
-                   lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
-                    lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
+                   ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
+                    ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
                     multi_ref_idx, x0, y0,
                     log2_pb_width, log2_pb_height);
                 // FIXED? :fill_ref_above_0_mref(src, dst_stride, ref1,
-                //                       lc_ctx->progress_map.rows[y0 >> 2],
-                //         lc_ctx->progress_map.cols[x0 >> 2],
+                //                       ctudec->progress_map.rows[y0 >> 2],
+                //         ctudec->progress_map.cols[x0 >> 2],
                 //         multi_ref_idx, x0 , y0,
                 //         log2_pb_width, log2_pb_height);
           fill_ref_above_0_mref(src, dst_stride, ref1,
-                  lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
-                  lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
+                  ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
+                  ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
                   multi_ref_idx, x0 , y0,
                   log2_pb_width, log2_pb_height);
 
@@ -1038,13 +1038,13 @@ fill_ref_left_0_mref(src, dst_stride, ref2,
 
 
 void
-vvc_intra_pred_mip(const OVCTUDec *const lc_ctx,
+vvc_intra_pred_mip(const OVCTUDec *const ctudec,
                    uint16_t *const dst,
                    int x0, int y0, int log2_pu_w, int log2_pu_h,
                    uint8_t mip_mode)
 {
-  // FIXED? :const uint16_t *src = &lc_ctx->ctu_data_y[VVC_CTB_OFFSET];
-  const uint16_t *src = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET];
+  // FIXED? :const uint16_t *src = &ctudec->ctu_data_y[VVC_CTB_OFFSET];
+  const uint16_t *src = &ctudec->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET];
 
     int32_t bndy_line[8];//buffer used to store averaged boundaries use int
     int16_t mip_pred[64];//buffer used to store reduced matrix vector results
@@ -1055,20 +1055,20 @@ vvc_intra_pred_mip(const OVCTUDec *const lc_ctx,
 
     int dst_stride = VVC_CTB_STRIDE;
 // FIXED? :fill_ref_left_0(src, dst_stride, ref_lft,
-//                 lc_ctx->progress_map.cols[x0 >> 2],
-//                 lc_ctx->progress_map.rows[y0 >> 2],
+//                 ctudec->progress_map.cols[x0 >> 2],
+//                 ctudec->progress_map.rows[y0 >> 2],
 //                 x0, y0, log2_pu_w, log2_pu_h, 0);
 // FIXED? :fill_ref_above_0(src, dst_stride, ref_abv,
-//                  lc_ctx->progress_map.rows[y0 >> 2],
-//                  lc_ctx->progress_map.cols[x0 >> 2],
+//                  ctudec->progress_map.rows[y0 >> 2],
+//                  ctudec->progress_map.cols[x0 >> 2],
 //                  x0, y0, log2_pu_w, log2_pu_h, 0);
    fill_ref_left_0(src, dst_stride, ref_lft,
-                   lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
-                   lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
+                   ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
+                   ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
                    x0, y0, log2_pu_w, log2_pu_h, 0);
    fill_ref_above_0(src, dst_stride, ref_abv,
-                    lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
-                    lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
+                    ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
+                    ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
                     x0, y0, log2_pu_w, log2_pu_h, 0);
 
     //compute reduced boundaries
@@ -1190,7 +1190,7 @@ vvc_intra_pred_mip(const OVCTUDec *const lc_ctx,
 }
 
 void
-vvc_intra_pred_mip_tr(const OVCTUDec *const lc_ctx,
+vvc_intra_pred_mip_tr(const OVCTUDec *const ctudec,
                       uint16_t *const dst,
                       int x0, int y0, int log2_pu_w, int log2_pu_h,
                       uint8_t mip_mode)
@@ -1202,8 +1202,8 @@ vvc_intra_pred_mip_tr(const OVCTUDec *const lc_ctx,
 
     int rnd;
     int i, j;
-    // FIXED? :const uint16_t *src = &lc_ctx->ctu_data_y[VVC_CTB_OFFSET];
-    const uint16_t *src = &lc_ctx->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET];
+    // FIXED? :const uint16_t *src = &ctudec->ctu_data_y[VVC_CTB_OFFSET];
+    const uint16_t *src = &ctudec->rcn_ctx.rcn_ctu_buff.data_y[VVC_CTB_OFFSET];
 
     int32_t bndy_line[8]; //buffer used to store averaged boundaries use int
     int16_t mip_pred[64];//buffer used to store reduced matrix vector results
@@ -1214,22 +1214,22 @@ vvc_intra_pred_mip_tr(const OVCTUDec *const lc_ctx,
     int dst_stride = VVC_CTB_STRIDE;
 
     // FIXED? :fill_ref_left_0(src, dst_stride, ref_lft,
-    //                 lc_ctx->progress_map.cols[x0 >> 2],
-    //                 lc_ctx->progress_map.rows[y0 >> 2],
+    //                 ctudec->progress_map.cols[x0 >> 2],
+    //                 ctudec->progress_map.rows[y0 >> 2],
     //                 x0, y0, log2_pu_w, log2_pu_h, 0);
     //
     // FIXED? :fill_ref_above_0(src, dst_stride, ref_abv,
-    //                  lc_ctx->progress_map.rows[y0 >> 2],
-    //                  lc_ctx->progress_map.cols[x0 >> 2],
+    //                  ctudec->progress_map.rows[y0 >> 2],
+    //                  ctudec->progress_map.cols[x0 >> 2],
     //                  x0, y0, log2_pu_w, log2_pu_h, 0);
    fill_ref_left_0(src, dst_stride, ref_lft,
-                   lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
-                   lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
+                   ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
+                   ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
                    x0, y0, log2_pu_w, log2_pu_h, 0);
 
    fill_ref_above_0(src, dst_stride, ref_abv,
-                    lc_ctx->rcn_ctx.progress_field.hfield[y0 >> 2],
-                    lc_ctx->rcn_ctx.progress_field.vfield[x0 >> 2],
+                    ctudec->rcn_ctx.progress_field.hfield[y0 >> 2],
+                    ctudec->rcn_ctx.progress_field.vfield[x0 >> 2],
                     x0, y0, log2_pu_w, log2_pu_h, 0);
 
     uint8_t log2_bnd_x = log2_pu_w - log2_bndy;
