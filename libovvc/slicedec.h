936c3988b730e987a8f3713f9d5001bb0d2b672b
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-02-12 16:34:34 +0100

    Add DRVLines structure to Slice Dec structure

--- libovvc/slicedec.h
+++ libovvc/slicedec.h
@@ -4,7 +4,9 @@
 #include "ovdefs.h"
 #include "ctudec.h"
 
-struct CCLines{
+
+struct CCLines
+{
     uint8_t *qt_depth_map_x;
     uint8_t *log2_cu_w_map_x;
     uint8_t *cu_mode_x;
@@ -14,10 +16,30 @@ struct CCLines{
      */
 };
 
+/* Structure used to retrieve above modes information for modes
+ * derivation
+ * FIXME realloc on picture width changes
+ */
+struct DRVLines
+{
+    /* Used for intra Most Probable Mode changes
+     * Init value is set to PLANAR
+     */
+    uint8_t *intra_luma_x;
+
+    /* Bit Field information on above reconstructed PU
+     * Used for Intra reference construction
+     * LSB correspond to first above PU
+     */
+    uint32_t *progress_map;
+};
+
 typedef struct OVSliceDec
 {
    /* Lins for CABAC context derivation luma and chroma */
    struct CCLines cabac_lines[2];
+
+   struct DRVLines drv_lines;
 
    OVCTUDec *ctudec_list; 
    int nb_ctudec;
