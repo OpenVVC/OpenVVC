ab60e45012a4e6e7559a99a8941da38031c38c28
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-03-27 10:52:57 +0100

    Use cat instead of echo to generate version.h file + fix revision typo

--- version.sh
+++ version.sh
@@ -5,17 +5,19 @@ VERSION=($(sed 's/\./\n/g' $1))
 
 MAJOR=${VERSION[0]}
 MINOR=${VERSION[1]}
-REVISON=${VERSION[2]}
+REVISION=${VERSION[2]}
 
 # Try to recover short SHA1 of current commit
 BUILD=$(git rev-parse --short HEAD 2>/dev/null || echo "")
 
-echo "#ifndef OVVERSION_H
+cat > $2 << EOF
+#ifndef OVVERSION_H
 #define OVVERSION_H
 
 #define VER_MAJOR ${MAJOR}
 #define VER_MINOR ${MINOR}
-#define VER_REVISION ${REVISON}
-#define VER_BUILD \"${BUILD}\"
+#define VER_REVISION ${REVISION}
+#define VER_BUILD "${BUILD}"
 
-#endif // OVVERSION_H" > $2
+#endif // OVVERSION_H
+EOF
