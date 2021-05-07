36a7111b3f931e57ae05a781edc67d012ffea09d
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-05-07 15:11:52 +0200

    Saturate ref_idx based for default cand based on nb_active pic minus 1

--- libovvc/drv_mvp.c
+++ libovvc/drv_mvp.c
@@ -1427,7 +1427,7 @@ vvc_derive_merge_mvp_b(const struct InterDRVCtx *const inter_ctx,
         nb_cand++;
     }
     /*FIXME saturation or ridx based on nb_active_ref*/
-    int8_t ridx = OVMIN(merge_idx - nb_cand, OVMIN(inter_ctx->nb_active_ref0, inter_ctx->nb_active_ref1));
+    int8_t ridx = OVMIN(merge_idx - nb_cand, OVMIN(inter_ctx->nb_active_ref0 - 1, inter_ctx->nb_active_ref1 - 1));
 
     mv_z.inter_dir = 3;
     mv_z.mv0.ref_idx = ridx;
