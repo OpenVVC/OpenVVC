7a3c72754fc2edeb39c29dbc146641ff9ae5f53c
gugautie <gugautie@insa-rennes.fr>
2021-07-15 16:06:25 +0200

    DC PLANAR Neon Optimisation added

--- libovvc/arm/armobj.mak
+++ libovvc/arm/armobj.mak
@@ -1,2 +1,3 @@
 arm_SRC_FOLDER:=libovvc/arm/
-arm_LIB_SRC:=  rcn_sao_neon.c\
+arm_LIB_SRC:=  	rcn_sao_neon.c										\
+								rcn_intra_dc_planar_neon.c				\
--- libovvc/arm/rcn_neon.h
+++ libovvc/arm/rcn_neon.h
@@ -2,6 +2,7 @@
 #define RCN_SSE_H
 #include "rcn_structures.h"
 
+void rcn_init_dc_planar_functions_neon(struct RCNFunctions *const rcn_funcs);
 void rcn_init_sao_functions_neon(struct RCNFunctions *const rcn_funcs);
 
 
