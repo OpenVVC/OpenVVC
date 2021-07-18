e30496db929c426a18b4c20311d65a4b98b7ddc6
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-07-18 19:04:08 +0200

    Clean DBF QP storage i coding unit

--- libovvc/vcl_coding_unit.c
+++ libovvc/vcl_coding_unit.c
@@ -703,46 +703,47 @@ coding_unit(OVCTUDec *const ctu_dec,
     }
 
     /* update dqp for deblocking filter usage */
-    if (ctu_dec->coding_tree != dual_tree) {
+    if (!ctu_dec->dbf_disable && !(cu.cu_flags & flg_isp_flag)) {
         struct DBFInfo *dbf_info = &ctu_dec->dbf_info;
-        fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
+        /* FIXME check separate tree */
+        if (ctu_dec->coding_unit == &coding_unit_intra) {
+            uint8_t qp_l  = ctu_dec->qp_ctx.current_qp;
 
+            fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
+            dbf_fill_qp_map(&dbf_info->qp_map_y, x0, y0, log2_cb_w, log2_cb_h, qp_l);
+        } else if (ctu_dec->coding_unit == &coding_unit_intra_c) {
+            uint8_t qp_cb = (ctu_dec->dequant_chroma == &ctu_dec->dequant_joint_cb_cr) ?
+                ctu_dec->dequant_joint_cb_cr.qp - 12 :
+                ctu_dec->dequant_cb.qp - 12;
 
-        fill_ctb_bound_c(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
+            uint8_t qp_cr = (ctu_dec->dequant_chroma == &ctu_dec->dequant_joint_cb_cr) ?
+                ctu_dec->dequant_joint_cb_cr.qp - 12 :
+                ctu_dec->dequant_cb.qp - 12;
 
-        dbf_fill_qp_map(&dbf_info->qp_map_y, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->qp_ctx.current_qp);
-        dbf_fill_qp_map(&dbf_info->qp_map_cb, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->dequant_cb.qp - 12);
-        dbf_fill_qp_map(&dbf_info->qp_map_cr, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->dequant_cr.qp - 12);
-    } else {
-        if (!ctu_dec->dbf_disable && !(cu.cu_flags & flg_isp_flag)) {
-            struct DBFInfo *dbf_info = &ctu_dec->dbf_info;
-            dbf_fill_qp_map(&dbf_info->qp_map_y, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->qp_ctx.current_qp);
-            /* FIXME separate tree */
-            if (ctu_dec->coding_unit == &coding_unit_intra) {
-                fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
-            } else if (ctu_dec->coding_unit == &coding_unit_intra_c) {
-                if (ctu_dec->dequant_chroma == &ctu_dec->dequant_joint_cb_cr) {
-                    dbf_fill_qp_map(&dbf_info->qp_map_cb, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1, ctu_dec->dequant_joint_cb_cr.qp - 12);
-                    dbf_fill_qp_map(&dbf_info->qp_map_cr, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1, ctu_dec->dequant_joint_cb_cr.qp - 12);
-                } else {
-                    dbf_fill_qp_map(&dbf_info->qp_map_cb, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1, ctu_dec->dequant_cb.qp - 12);
-                    dbf_fill_qp_map(&dbf_info->qp_map_cr, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1, ctu_dec->dequant_cr.qp - 12);
-                }
-                fill_ctb_bound_c(&ctu_dec->dbf_info, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1);
+            dbf_fill_qp_map(&dbf_info->qp_map_cb, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1, qp_cb);
+            dbf_fill_qp_map(&dbf_info->qp_map_cr, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1, qp_cr);
+            fill_ctb_bound_c(&ctu_dec->dbf_info, x0 << 1, y0 << 1, log2_cb_w + 1, log2_cb_h + 1);
 
-            } else {
-                fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
-
-                fill_ctb_bound_c(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
-
-                dbf_fill_qp_map(&dbf_info->qp_map_cb, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->dequant_cb.qp - 12);
-                dbf_fill_qp_map(&dbf_info->qp_map_cr, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->dequant_cr.qp - 12);
-                dbf_fill_qp_map(&dbf_info->qp_map_cb, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->dequant_joint_cb_cr.qp - 12);
-                dbf_fill_qp_map(&dbf_info->qp_map_cr, x0, y0, log2_cb_w, log2_cb_h, ctu_dec->dequant_joint_cb_cr.qp - 12);
-            }
         } else {
-            //fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
+            uint8_t qp_l  = ctu_dec->qp_ctx.current_qp;
+            uint8_t qp_cb = (ctu_dec->dequant_chroma == &ctu_dec->dequant_joint_cb_cr) ?
+                ctu_dec->dequant_joint_cb_cr.qp - 12 :
+                ctu_dec->dequant_cb.qp - 12;
+
+            uint8_t qp_cr = (ctu_dec->dequant_chroma == &ctu_dec->dequant_joint_cb_cr) ?
+                ctu_dec->dequant_joint_cb_cr.qp - 12 :
+                ctu_dec->dequant_cb.qp - 12;
+
+            fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
+
+            fill_ctb_bound_c(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
+
+            dbf_fill_qp_map(&dbf_info->qp_map_y, x0, y0, log2_cb_w, log2_cb_h, qp_l);
+            dbf_fill_qp_map(&dbf_info->qp_map_cb, x0, y0, log2_cb_w, log2_cb_h, qp_cb);
+            dbf_fill_qp_map(&dbf_info->qp_map_cr, x0, y0, log2_cb_w, log2_cb_h, qp_cr);
         }
+    } else {
+            //fill_ctb_bound(&ctu_dec->dbf_info, x0, y0, log2_cb_w, log2_cb_h);
     }
 
     // Update depth_maps to selected depths
