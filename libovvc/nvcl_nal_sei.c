e136be6e055fba7e82523a571962638ba1a33052
Thomas INSA <tamestoy@pc-eii181>
2021-07-30 16:35:30 +0200

    SLHDR: read correctly sei payload

--- libovvc/nvcl_nal_sei.c
+++ libovvc/nvcl_nal_sei.c
@@ -142,6 +142,43 @@ nvcl_film_grain_read(OVNVCLReader *const rdr, struct OVSEIFGrain *const fg, OVNV
   } // cancel flag
 }
 
+// void xParseSEIUserDataRegistered(SEIUserDataRegistered& sei, uint32_t payloadSize, std::ostream *pDecodedMessageOutputStream)
+//TODOhdr: creer structure
+void
+nvcl_slhdr_read(OVNVCLReader *const rdr,  uint32_t payloadSize)
+{
+  // sei_read_code(pDecodedMessageOutputStream, 8, code, "itu_t_t35_country_code"); payloadSize--;
+  uint32_t code = nvcl_read_bits(rdr, 8);
+  payloadSize--;
+
+  if (code == 255){
+    // sei_read_code(pDecodedMessageOutputStream, 8, code, "itu_t_t35_country_code_extension_byte"); 
+    code = nvcl_read_bits(rdr, 8);
+    payloadSize--;
+    code += 255;
+  }
+  uint32_t ituCountryCode = code;
+
+  // sei.m_userData.resize(payloadSize);
+  uint32_t* slhdr_data = ov_mallocz(sizeof(uint32_t) * (payloadSize + 1));
+
+  for (int i = 0; i < payloadSize; i++){
+    // sei_read_code(NULL, 8, code, "itu_t_t35_payload_byte");
+    slhdr_data[i] = nvcl_read_bits(rdr, 8);
+  }
+
+    // SLHDR detected - reinsert country code at beginning of payload
+  if (ituCountryCode==0xB5 && slhdr_data[0] == 0x00 && slhdr_data[1] == 0x3A) {
+    // sei.m_userData.resize(payloadSize+1);
+    for (int i= payloadSize; i>0; i--){
+      slhdr_data[i] = slhdr_data[i-1];
+    }
+    slhdr_data[0] = (uint8_t)ituCountryCode;
+  }
+}
+
+
+
 int 
 nvcl_decode_nalu_sei(OVNVCLReader *const rdr, OVNVCLCtx *const nvcl_ctx)
 {   
@@ -154,9 +191,14 @@ nvcl_decode_nalu_sei(OVNVCLReader *const rdr, OVNVCLCtx *const nvcl_ctx)
     {
         uint8_t sei_byte;
         case FILM_GRAIN_CHARACTERISTICS:
+            ov_log(NULL, OVLOG_DEBUG, "SEI: FILM_GRAIN_CHARACTERISTICS (type = %d) with size %d.\n", payload.type, payload.size);
             if(!sei->sei_fg)
                 sei->sei_fg = ov_mallocz(sizeof(struct OVSEIFGrain));
             nvcl_film_grain_read(rdr, sei->sei_fg, nvcl_ctx);
+            break;
+        case USER_DATA_REGISTERED_ITU_T_T35:
+            ov_log(NULL, OVLOG_DEBUG, "SEI: USER_DATA_REGISTERED_ITU_T_T35 (type = %d) with size %d.\n", payload.type, payload.size);
+            nvcl_slhdr_read(rdr,payload.size);
             break;
         default:
             for (int i = 0; i < payload.size; i++)
