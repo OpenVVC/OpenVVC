f460b0047d6aff820c661780ff36d291d7d9e285
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-08-20 15:03:59 +0200

    Cosmetics in init_loop_filters

--- libovvc/slicedec.c
+++ libovvc/slicedec.c
@@ -432,9 +432,9 @@ init_in_loop_filters(OVCTUDec *const ctudec, const OVPS *const prms)
     }
 
     //Init LMCS info and output pivots
-    struct LMCSInfo* lmcs_info  = &ctudec->lmcs_info;
+    struct LMCSInfo* lmcs_info   = &ctudec->lmcs_info;
     lmcs_info->lmcs_enabled_flag = ph->ph_lmcs_enabled_flag;
-    lmcs_info->scale_c_flag = ph->ph_chroma_residual_scale_flag;
+    lmcs_info->scale_c_flag      = ph->ph_chroma_residual_scale_flag;
     if(sh->sh_lmcs_used_flag){
         int bitdepth = 10;
         if(!lmcs_info->lmcs_lut_inv_luma){
