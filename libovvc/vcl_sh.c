e1bda71c8c5a0f7a54d69152dc223dc2c5803f8a
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-08-27 18:48:25 +0200

    Do not set num_active_ref_entries on RPL1 when slice is not B

--- libovvc/vcl_sh.c
+++ libovvc/vcl_sh.c
@@ -221,9 +221,11 @@ nvcl_sh_read(OVNVCLReader *const rdr, OVSH *const sh,
     /* FIXME do this in Info structures ?*/
     if (sh->sh_slice_type != I) {
         sh->hrpl.rpl_h0.rpl_data.num_ref_active_entries = nb_ref_entries0;
-        sh->hrpl.rpl_h1.rpl_data.num_ref_active_entries = nb_ref_entries1;
         ph->hrpl.rpl_h0.rpl_data.num_ref_active_entries = nb_ref_entries0;
-        ph->hrpl.rpl_h1.rpl_data.num_ref_active_entries = nb_ref_entries1;
+        if (sh->sh_slice_type == B) {
+            sh->hrpl.rpl_h1.rpl_data.num_ref_active_entries = nb_ref_entries1;
+            ph->hrpl.rpl_h1.rpl_data.num_ref_active_entries = nb_ref_entries1;
+        }
     } else {
         sh->hrpl.rpl_h0.rpl_data.num_ref_active_entries = 0;
         sh->hrpl.rpl_h1.rpl_data.num_ref_active_entries = 0;
