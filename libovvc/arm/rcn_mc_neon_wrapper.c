07ce3640221a16ea38bbd2b1d5687fec2571b0c7
Gautier Guillaume <guillaume.gautier@insa-rennes.fr>
2021-09-07 20:02:00 +0200

    MC: EPEL H Neon Optimisation

--- libovvc/arm/rcn_mc_neon_wrapper.c
+++ libovvc/arm/rcn_mc_neon_wrapper.c
@@ -252,30 +252,39 @@ void ov_put_vvc_bi1_qpel_hv_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststr
 
 void ov_put_vvc_uni_epel_h_10_8_neon();
 void ov_put_vvc_uni_epel_h_10_16_neon();
+void ov_put_vvc_uni_epel_h_10_32_neon();
 
 void ov_put_vvc_uni_epel_v_10_8_neon();
 void ov_put_vvc_uni_epel_v_10_16_neon();
+void ov_put_vvc_uni_epel_v_10_32_neon();
 
 void ov_put_vvc_uni_epel_hv_10_8_neon();
 void ov_put_vvc_uni_epel_hv_10_16_neon();
+void ov_put_vvc_uni_epel_hv_10_32_neon();
 
 void ov_put_vvc_bi0_epel_h_10_8_neon();
 void ov_put_vvc_bi0_epel_h_10_16_neon();
+void ov_put_vvc_bi0_epel_h_10_32_neon();
 
 void ov_put_vvc_bi0_epel_v_10_8_neon();
 void ov_put_vvc_bi0_epel_v_10_16_neon();
+void ov_put_vvc_bi0_epel_v_10_32_neon();
 
 void ov_put_vvc_bi0_epel_hv_10_8_neon();
 void ov_put_vvc_bi0_epel_hv_10_16_neon();
+void ov_put_vvc_bi0_epel_hv_10_32_neon();
 
 void ov_put_vvc_bi1_epel_h_10_8_neon();
 void ov_put_vvc_bi1_epel_h_10_16_neon();
+void ov_put_vvc_bi1_epel_h_10_32_neon();
 
 void ov_put_vvc_bi1_epel_v_10_8_neon();
 void ov_put_vvc_bi1_epel_v_10_16_neon();
+void ov_put_vvc_bi1_epel_v_10_32_neon();
 
 void ov_put_vvc_bi1_epel_hv_10_8_neon();
 void ov_put_vvc_bi1_epel_hv_10_16_neon();
+void ov_put_vvc_bi1_epel_hv_10_32_neon();
 
 void ov_put_vvc_uni_epel_h_10_8_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride,
                    const uint16_t* _src, ptrdiff_t _srcstride,
@@ -286,6 +295,11 @@ void ov_put_vvc_uni_epel_h_10_16_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststri
                   const uint16_t* _src, ptrdiff_t _srcstride,
                   int height, intptr_t mx, intptr_t my, int width){
                     ov_put_vvc_uni_epel_h_10_16_neon(_dst, _dststride, _src, _srcstride, height, ov_mcp_filters_c[mx - 1], width);
+                  }
+void ov_put_vvc_uni_epel_h_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride,
+                  const uint16_t* _src, ptrdiff_t _srcstride,
+                  int height, intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_uni_epel_h_10_32_neon(_dst, _dststride, _src, _srcstride, height, ov_mcp_filters_c[mx - 1], width);
                   }
 
 void ov_put_vvc_uni_epel_v_10_8_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride,
@@ -298,6 +312,11 @@ void ov_put_vvc_uni_epel_v_10_16_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststri
                   int height, intptr_t mx, intptr_t my, int width){
                     ov_put_vvc_uni_epel_v_10_16_neon(_dst, _dststride, _src, _srcstride, height, ov_mcp_filters_c[my - 1], width);
                   }
+void ov_put_vvc_uni_epel_v_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride,
+                  const uint16_t* _src, ptrdiff_t _srcstride,
+                  int height, intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_uni_epel_v_10_32_neon(_dst, _dststride, _src, _srcstride, height, ov_mcp_filters_c[my - 1], width);
+                  }
 
 void ov_put_vvc_uni_epel_hv_10_8_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride,
                    const uint16_t* _src, ptrdiff_t _srcstride,
@@ -309,6 +328,11 @@ void ov_put_vvc_uni_epel_hv_10_16_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststr
                   int height, intptr_t mx, intptr_t my, int width){
                     ov_put_vvc_uni_epel_hv_10_16_neon(_dst, _dststride, _src, _srcstride, height, ov_mcp_filters_c[mx - 1], ov_mcp_filters_c[my - 1], width);
                   }
+void ov_put_vvc_uni_epel_hv_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride,
+                  const uint16_t* _src, ptrdiff_t _srcstride,
+                  int height, intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_uni_epel_hv_10_32_neon(_dst, _dststride, _src, _srcstride, height, ov_mcp_filters_c[mx - 1], ov_mcp_filters_c[my - 1], width);
+                  }
 
 
 void ov_put_vvc_bi0_epel_h_10_8_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
@@ -316,9 +340,13 @@ void ov_put_vvc_bi0_epel_h_10_8_neon_wrapper(int16_t* _dst, const uint16_t* _src
                      ov_put_vvc_bi0_epel_h_10_8_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[mx-1], width);
                    }
 void ov_put_vvc_bi0_epel_h_10_16_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
-                  int height, intptr_t mx, intptr_t my, int width){
-                    ov_put_vvc_bi0_epel_h_10_16_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[mx-1], width);
-                  }
+                 int height, intptr_t mx, intptr_t my, int width){
+                   ov_put_vvc_bi0_epel_h_10_16_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[mx-1], width);
+                 }
+void ov_put_vvc_bi0_epel_h_10_32_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
+                 int height, intptr_t mx, intptr_t my, int width){
+                   ov_put_vvc_bi0_epel_h_10_32_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[mx-1], width);
+                 }
 
 void ov_put_vvc_bi0_epel_v_10_8_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
                   int height, intptr_t mx, intptr_t my, int width){
@@ -328,6 +356,10 @@ void ov_put_vvc_bi0_epel_v_10_16_neon_wrapper(int16_t* _dst, const uint16_t* _sr
                   int height, intptr_t mx, intptr_t my, int width){
                     ov_put_vvc_bi0_epel_v_10_16_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[my-1], width);
                   }
+void ov_put_vvc_bi0_epel_v_10_32_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
+                  int height, intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_bi0_epel_v_10_32_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[my-1], width);
+                  }
 
 void ov_put_vvc_bi0_epel_hv_10_8_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
                   int height, intptr_t mx, intptr_t my, int width){
@@ -336,6 +368,10 @@ void ov_put_vvc_bi0_epel_hv_10_8_neon_wrapper(int16_t* _dst, const uint16_t* _sr
 void ov_put_vvc_bi0_epel_hv_10_16_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
                   int height, intptr_t mx, intptr_t my, int width){
                     ov_put_vvc_bi0_epel_hv_10_16_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[mx-1], ov_mcp_filters_c[my-1], width);
+                  }
+void ov_put_vvc_bi0_epel_hv_10_32_neon_wrapper(int16_t* _dst, const uint16_t* _src, ptrdiff_t _srcstride,
+                  int height, intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_bi0_epel_hv_10_32_neon(_dst, _src, _srcstride, height, ov_mcp_filters_c[mx-1], ov_mcp_filters_c[my-1], width);
                   }
 
  void ov_put_vvc_bi1_epel_h_10_8_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride, const uint16_t* _src0,
@@ -348,6 +384,11 @@ void ov_put_vvc_bi0_epel_hv_10_16_neon_wrapper(int16_t* _dst, const uint16_t* _s
                    intptr_t mx, intptr_t my, int width){
                       ov_put_vvc_bi1_epel_h_10_16_neon(_dst, _dststride, _src0, _srcstride, _src1, height, ov_mcp_filters_c[mx-1], width);
                     }
+void ov_put_vvc_bi1_epel_h_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride, const uint16_t* _src0,
+                  ptrdiff_t _srcstride, const int16_t* _src1, int height,
+                  intptr_t mx, intptr_t my, int width){
+                     ov_put_vvc_bi1_epel_h_10_32_neon(_dst, _dststride, _src0, _srcstride, _src1, height, ov_mcp_filters_c[mx-1], width);
+                   }
 
 void ov_put_vvc_bi1_epel_v_10_8_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride, const uint16_t* _src0,
                   ptrdiff_t _srcstride, const int16_t* _src1, int height,
@@ -359,6 +400,11 @@ void ov_put_vvc_bi1_epel_v_10_16_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststri
                   intptr_t mx, intptr_t my, int width){
                      ov_put_vvc_bi1_epel_v_10_16_neon(_dst, _dststride, _src0, _srcstride, _src1, height, ov_mcp_filters_c[my-1], width);
                    }
+void ov_put_vvc_bi1_epel_v_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride, const uint16_t* _src0,
+                 ptrdiff_t _srcstride, const int16_t* _src1, int height,
+                 intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_bi1_epel_v_10_32_neon(_dst, _dststride, _src0, _srcstride, _src1, height, ov_mcp_filters_c[my-1], width);
+                  }
 
 void ov_put_vvc_bi1_epel_hv_10_8_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride, const uint16_t* _src0,
                  ptrdiff_t _srcstride, const int16_t* _src1, int height,
@@ -369,6 +415,11 @@ void ov_put_vvc_bi1_epel_hv_10_16_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststr
                  ptrdiff_t _srcstride, const int16_t* _src1, int height,
                  intptr_t mx, intptr_t my, int width){
                     ov_put_vvc_bi1_epel_hv_10_16_neon(_dst, _dststride, _src0, _srcstride, _src1, height, ov_mcp_filters_c[mx-1], ov_mcp_filters_c[my-1], width);
+                  }
+void ov_put_vvc_bi1_epel_hv_10_32_neon_wrapper(uint16_t* _dst, ptrdiff_t _dststride, const uint16_t* _src0,
+                 ptrdiff_t _srcstride, const int16_t* _src1, int height,
+                 intptr_t mx, intptr_t my, int width){
+                    ov_put_vvc_bi1_epel_hv_10_32_neon(_dst, _dststride, _src0, _srcstride, _src1, height, ov_mcp_filters_c[mx-1], ov_mcp_filters_c[my-1], width);
                   }
 
 void
@@ -407,7 +458,7 @@ rcn_init_mc_functions_neon(struct RCNFunctions* const rcn_funcs)
   //
   mc_l->unidir[3][SIZE_BLOCK_16] = &ov_put_vvc_uni_qpel_hv_10_16_neon_wrapper;
   mc_l->bidir0[3][SIZE_BLOCK_16] = &ov_put_vvc_bi0_qpel_hv_10_16_neon_wrapper;
-  // mc_l->bidir1[3][SIZE_BLOCK_16] = &dav1d_ff_hevc_transform_4x4_neon_8;
+  mc_l->bidir1[3][SIZE_BLOCK_16] = &ov_put_vvc_bi1_qpel_hv_10_16_neon_wrapper;
   //
   mc_l->bidir0[0][SIZE_BLOCK_32] = &ov_put_vvc_bi0_pel_pixels_10_32_neon;
   mc_l->bidir1[0][SIZE_BLOCK_32] = &ov_put_vvc_bi1_pel_pixels_10_32_neon;
@@ -422,7 +473,7 @@ rcn_init_mc_functions_neon(struct RCNFunctions* const rcn_funcs)
   //
   mc_l->unidir[3][SIZE_BLOCK_32] = &ov_put_vvc_uni_qpel_hv_10_32_neon_wrapper;
   mc_l->bidir0[3][SIZE_BLOCK_32] = &ov_put_vvc_bi0_qpel_hv_10_32_neon_wrapper;
-  // mc_l->bidir1[3][SIZE_BLOCK_32] = &dav1d_ff_hevc_transform_4x4_neon_8;
+  mc_l->bidir1[3][SIZE_BLOCK_32] = &ov_put_vvc_bi1_qpel_hv_10_32_neon_wrapper;
 
   /* Chroma functions */
   mc_c->bidir0[0][SIZE_BLOCK_4] = &ov_put_vvc_bi0_pel_pixels_10_4_neon;
@@ -469,4 +520,19 @@ rcn_init_mc_functions_neon(struct RCNFunctions* const rcn_funcs)
   mc_c->unidir[3][SIZE_BLOCK_16] = &ov_put_vvc_uni_epel_hv_10_16_neon_wrapper;
   mc_c->bidir0[3][SIZE_BLOCK_16] = &ov_put_vvc_bi0_epel_hv_10_16_neon_wrapper;
   mc_c->bidir1[3][SIZE_BLOCK_16] = &ov_put_vvc_bi1_epel_hv_10_16_neon_wrapper;
+
+  // mc_c->bidir0[0][SIZE_BLOCK_32] = &ov_put_vvc_bi0_pel_pixels_10_16_neon;
+  // mc_c->bidir1[0][SIZE_BLOCK_32] = &ov_put_vvc_bi1_pel_pixels_10_16_neon;
+  //
+  mc_c->unidir[1][SIZE_BLOCK_32] = &ov_put_vvc_uni_epel_h_10_32_neon_wrapper;
+  mc_c->bidir0[1][SIZE_BLOCK_32] = &ov_put_vvc_bi0_epel_h_10_32_neon_wrapper;
+  mc_c->bidir1[1][SIZE_BLOCK_32] = &ov_put_vvc_bi1_epel_h_10_32_neon_wrapper;
+  //
+  mc_c->unidir[2][SIZE_BLOCK_32] = &ov_put_vvc_uni_epel_v_10_32_neon_wrapper;
+  mc_c->bidir0[2][SIZE_BLOCK_32] = &ov_put_vvc_bi0_epel_v_10_32_neon_wrapper;
+  mc_c->bidir1[2][SIZE_BLOCK_32] = &ov_put_vvc_bi1_epel_v_10_32_neon_wrapper;
+  //
+  mc_c->unidir[3][SIZE_BLOCK_32] = &ov_put_vvc_uni_epel_hv_10_32_neon_wrapper;
+  mc_c->bidir0[3][SIZE_BLOCK_32] = &ov_put_vvc_bi0_epel_hv_10_32_neon_wrapper;
+  mc_c->bidir1[3][SIZE_BLOCK_32] = &ov_put_vvc_bi1_epel_hv_10_32_neon_wrapper;
 }
