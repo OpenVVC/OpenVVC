b9baee29840eb51419ee9f89c5d2c1e98b628978
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-10-06 11:31:02 +0200

    Add missing new lines at end of files

--- libovvc/post_proc.h
+++ libovvc/post_proc.h
@@ -31,3 +31,4 @@ void fg_grain_apply_pic(int16_t** dstComp, int16_t** srcComp, struct OVSEIFGrain
 void fg_grain_no_filter(int16_t** dstComp, int16_t** srcComp, struct OVSEIFGrain* fgrain, 
                           int pic_w, int pic_h, int poc, uint8_t isIdrPic, uint8_t enableDeblocking);
 #endif
+
