6bbfe1eee20d5dcec7d3f61fc09b2a006347d86a
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-10-21 13:36:09 +0200

    Document ovframe functions

--- libovvc/ovframe.h
+++ libovvc/ovframe.h
@@ -3,10 +3,9 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include "ovdefs.h"
 #include <stdatomic.h>
+#include "ovdefs.h"
 
-/* TODO Decide on values */
 enum ChromaFmt
 {
     OV_YUV_420_P8  = 0,
@@ -17,14 +16,11 @@ enum ChromaFmt
 struct FrameInfo
 {
     enum ChromaFmt chromat_format;
-
-    /* TODO add VUI info
-     *      add RefPicList info
-     */
 };
 
 struct FramePool;
 
+/* OVFrame private data */
 struct FrameInternal
 {
     /* reference counter */
@@ -37,28 +33,53 @@ struct FrameInternal
 
 struct Frame
 {
-    /* Pointer to Picture data planes per component */
+    /* Pointer to Picture data planes per component
+     */
     uint8_t *data[3];
 
-    /* Per component line size in bytes */
+    /* Per component line size in bytes
+     */
     size_t linesize[3];
+
+    /* Per components width and height in samples
+     */
     size_t width[3];
     size_t height[3];
 
-    /* Picture Order Count */
+    /* Picture Order Count
+     */
     uint32_t poc;
 
-    /* Presentation Timestamp */
+    /* Presentation Time Stamp
+     */
     uint64_t pts;
 
     struct FrameInfo frame_info;
 
-    /* private data */
+    /* OVFrame Private data
+     * Do not modify.
+     */
     struct FrameInternal internal;
 };
 
-int ovframe_new_ref(OVFrame **dst, OVFrame *src);
+/* Reference an OVFrame pointer
+ *
+ * Add a new reference to an OVFrame pointed by src and increase its
+ * internal reference counter.
+ *
+ * Note:
+ *     - Do not call if the value pointed by dst_p already
+ *     references an OVFrame
+ *     - src must be a valid OVFrame (i.e. from the decoder output)
+ */
+int ovframe_new_ref(OVFrame **dst_p, OVFrame *src);
 
-void ovframe_unref(OVFrame **frame);
+/* Dereference an OVFrame pointer
+ *
+ * Decrements reference counter of the OVFrame pointed by ovframe_p
+ * and set ovframe_p to NULL. If the reference counter drops down to zero
+ * the OVFrame pointed by ovframe_p will be freed.
+ */
+void ovframe_unref(OVFrame **frame_p);
 
 #endif
