cfc88b275413ab41deaa7caa934f99372c6c4db0
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-11-10 16:55:39 +0100

    Harmonize variables names in Intra DC/PLANAR functions

--- libovvc/rcn_intra_dc_planar.c
+++ libovvc/rcn_intra_dc_planar.c
@@ -29,81 +29,81 @@ static const uint8_t vvc_pdpc_w[3][128] = {
 };
 
 void
-vvc_intra_dc(const uint16_t* const src_above, const uint16_t* const src_left,
-             uint16_t* const dst, ptrdiff_t dst_stride, int log2_pb_width,
-             int log2_pb_height)
+vvc_intra_dc(const uint16_t* const ref_abv, const uint16_t* const ref_lft,
+             uint16_t* const dst, ptrdiff_t dst_stride, int log2_pb_w,
+             int log2_pb_h)
 {
     int idx;
     uint16_t* _dst = dst;
-    int dc_value = 0;
+    int dc_val = 0;
 
-    const int denom = (log2_pb_width == log2_pb_height)
-        ? (log2_pb_width + 1)
-        : OVMAX(log2_pb_width, log2_pb_height);
+    const int denom = (log2_pb_w == log2_pb_h)
+        ? (log2_pb_w + 1)
+        : OVMAX(log2_pb_w, log2_pb_h);
     const int shift = denom;
     const int offset = ((1 << denom) >> 1);
 
-    if (log2_pb_width >= log2_pb_height) {
-        for (idx = 0; idx < (1 << log2_pb_width); idx++) {
-            dc_value += src_above[1 + idx];
+    if (log2_pb_w >= log2_pb_h) {
+        for (idx = 0; idx < (1 << log2_pb_w); idx++) {
+            dc_val += ref_abv[1 + idx];
         }
     }
-    if (log2_pb_width <= log2_pb_height) {
-        for (idx = 0; idx < (1 << log2_pb_height); idx++) {
-            dc_value += src_left[1 + idx];
+    if (log2_pb_w <= log2_pb_h) {
+        for (idx = 0; idx < (1 << log2_pb_h); idx++) {
+            dc_val += ref_lft[1 + idx];
         }
     }
 
-    dc_value = (dc_value + offset) >> shift;
+    dc_val = (dc_val + offset) >> shift;
 
-    for (int i = 0; i < (1 << log2_pb_height); ++i) {
-        for (int j = 0; j < (1 << log2_pb_width); j++) {
-            _dst[j] = dc_value;
+    for (int i = 0; i < (1 << log2_pb_h); ++i) {
+        for (int j = 0; j < (1 << log2_pb_w); j++) {
+            _dst[j] = dc_val;
         }
         _dst += dst_stride;
     }
 }
 
 void
-vvc_intra_planar(const uint16_t* const src_above,
-                 const uint16_t* const src_left, uint16_t* const dst,
-                 ptrdiff_t dst_stride, int log2_pb_width, int log2_pb_height)
+vvc_intra_planar(const uint16_t* const ref_abv,
+                 const uint16_t* const ref_lft, uint16_t* const dst,
+                 ptrdiff_t dst_stride, int log2_pb_w, int log2_pb_h)
 {
 
     uint16_t* _dst = dst;
-    const uint32_t width = 1 << log2_pb_width;
-    const uint32_t height = 1 << log2_pb_height;
-    const uint32_t shift = 1 + log2_pb_width + log2_pb_height;
-    const uint32_t offset = 1 << (log2_pb_width + log2_pb_height);
+    const uint32_t pb_w = 1 << log2_pb_w;
+    const uint32_t pb_h = 1 << log2_pb_h;
+    const uint32_t shift = 1 + log2_pb_w + log2_pb_h;
+    const uint32_t offset = 1 << (log2_pb_w + log2_pb_h);
     int value;
 
     int top_row[128], bottom_row[128];
-    int top_right = src_above[width + 1];
+    int top_right = ref_abv[pb_w + 1];
 
-    for (int j = 0; j < width + 1; j++) {
-        bottom_row[j] = src_left[height + 1];
+    for (int j = 0; j < pb_w + 1; j++) {
+        bottom_row[j] = ref_lft[pb_h + 1];
     }
 
-    for (int k = 0; k < width; k++) {
-        value = src_above[k + 1];
+    for (int k = 0; k < pb_w; k++) {
+        value = ref_abv[k + 1];
         bottom_row[k] -= value; // bottom_left - val
-        top_row[k] = value << log2_pb_height;
+        top_row[k] = value << log2_pb_h;
     }
 
-    for (int y = 0; y < height; y++) {
-        int value = (int)src_left[y + 1]; // left_value y
-        int hor_pred = value << log2_pb_width;
+    for (int y = 0; y < pb_h; y++) {
+        int value = (int)ref_lft[y + 1]; // left_value y
+        int hor_pred = value << log2_pb_w;
         int right_pred = top_right - value;
 
-        for (int x = 0; x < width; x++) {
+        for (int x = 0; x < pb_w; x++) {
             int vertPred;
             hor_pred += right_pred;
             top_row[x] += bottom_row[x];
 
             vertPred = top_row[x];
 
-            _dst[x] = ((hor_pred << log2_pb_height) +
-                       (vertPred << log2_pb_width) + offset) >>
+            _dst[x] = ((hor_pred << log2_pb_h) +
+                       (vertPred << log2_pb_w) + offset) >>
                 shift;
         }
         _dst += dst_stride;
@@ -111,8 +111,8 @@ vvc_intra_planar(const uint16_t* const src_above,
 }
 
 void
-vvc_intra_dc_pdpc(const uint16_t* const src_above,
-                  const uint16_t* const src_left, uint16_t* const dst,
+vvc_intra_dc_pdpc(const uint16_t* const ref_abv,
+                  const uint16_t* const ref_lft, uint16_t* const dst,
                   ptrdiff_t dst_stride, int log2_pb_w, int log2_pb_h)
 {
     int idx;
@@ -129,12 +129,12 @@ vvc_intra_dc_pdpc(const uint16_t* const src_above,
 
     if (log2_pb_w >= log2_pb_h) {
         for (idx = 0; idx < (1 << log2_pb_w); idx++) {
-            dc_val += src_above[1 + idx];
+            dc_val += ref_abv[1 + idx];
         }
     }
     if (log2_pb_w <= log2_pb_h) {
         for (idx = 0; idx < (1 << log2_pb_h); idx++) {
-            dc_val += src_left[1 + idx];
+            dc_val += ref_lft[1 + idx];
         }
     }
 
@@ -143,9 +143,9 @@ vvc_intra_dc_pdpc(const uint16_t* const src_above,
     for (int y = 0; y < (1 << log2_pb_h); y++) {
         int x;
         int l_wgh = pdpc_w[y];
-        const int16_t l_val = src_left[y + 1];
+        const int16_t l_val = ref_lft[y + 1];
         for (x = 0; x < (1 << log2_pb_w); x++) {
-            const int16_t t_val = src_above[x + 1];
+            const int16_t t_val = ref_abv[x + 1];
             int t_wgh = pdpc_w[x];
             int val = ((t_wgh * l_val) + (l_wgh * t_val) +
                        (64 - (t_wgh + l_wgh)) * dc_val + 32) >>
@@ -157,14 +157,14 @@ vvc_intra_dc_pdpc(const uint16_t* const src_above,
 }
 
 void
-vvc_intra_planar_pdpc(const uint16_t* const src_above,
-                      const uint16_t* const src_left, uint16_t* const dst,
+vvc_intra_planar_pdpc(const uint16_t* const ref_abv,
+                      const uint16_t* const ref_lft, uint16_t* const dst,
                       ptrdiff_t dst_stride, int log2_pb_w, int log2_pb_h)
 {
 
     uint16_t* _dst = dst;
-    const uint32_t width = 1 << log2_pb_w;
-    const uint32_t height = 1 << log2_pb_h;
+    const uint32_t pb_w = 1 << log2_pb_w;
+    const uint32_t pb_h = 1 << log2_pb_h;
     const uint32_t w_scale = OVMAX(1, log2_pb_w);
     const uint32_t h_scale = OVMAX(1, log2_pb_h);
     const uint32_t s_shift = w_scale + h_scale + 1;
@@ -174,26 +174,26 @@ vvc_intra_planar_pdpc(const uint16_t* const src_above,
 
     int32_t t_row[128], b_row[128];
     const int8_t* pdpc_w = (int8_t *)vvc_pdpc_w[pdpc_scale];
-    const int16_t bl_val = src_left[height + 1];
-    const int16_t tr_val = src_above[width + 1];
+    const int16_t bl_val = ref_lft[pb_h + 1];
+    const int16_t tr_val = ref_abv[pb_w + 1];
     int y;
 
-    for (y = 0; y < width; ++y) {
-        int32_t t_val = (int32_t)src_above[y + 1];
+    for (y = 0; y < pb_w; ++y) {
+        int32_t t_val = (int32_t)ref_abv[y + 1];
         b_row[y] = bl_val - t_val;
         t_row[y] = t_val << h_scale;
     }
 
-    for (y = 0; y < height; ++y) {
+    for (y = 0; y < pb_h; ++y) {
         int x;
-        int32_t l_val = (int32_t)src_left[y + 1];
+        int32_t l_val = (int32_t)ref_lft[y + 1];
         int y_wgh = pdpc_w[y];
         int32_t r_col_y = tr_val - l_val;
         int32_t l_col_y = l_val << w_scale;
-        for (x = 0; x < width; ++x) {
+        for (x = 0; x < pb_w; ++x) {
             int32_t val;
             int x_wgh = pdpc_w[x];
-            int32_t t_val = (int32_t)src_above[x + 1];
+            int32_t t_val = (int32_t)ref_abv[x + 1];
             l_col_y += r_col_y;
             t_row[x] += b_row[x];
             val = ((l_col_y << h_scale) + (t_row[x] << w_scale) +
