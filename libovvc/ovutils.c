dc50b3619acf3ec2f0f2cf25854e2844cd841915
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-11-12 16:42:34 +0100

    Force inlining of clipping functions

--- libovvc/ovutils.c
+++ libovvc/ovutils.c
@@ -9,36 +9,6 @@
 #endif
 
 /* FIXME check if used on negative numbers */
-uint32_t
-ov_clip_uintp2(int32_t val, uint32_t a)
-{
-    if (val > 0) {
-        int32_t mask  = (1 << a) - 1;
-        int32_t overflow = !!(val & (~mask));
-        return ((-overflow) & mask) | (val & mask); 
-    } else {
-        return 0;
-    }
-    #if 0
-    return OVMIN(OVMAX(0, val), (1 << a) - 1);
-    #endif
-}
-
-int32_t
-ov_clip_intp2(int32_t val, uint32_t a)
-{   
-    int b = a-1;
-    if (val > 0) {
-        int32_t mask  = (1 << b) - 1;
-        int32_t overflow = !!(val & (~mask));
-        return ((-overflow) & mask) | (val & mask); 
-    } else {
-        val = -val;
-        int32_t mask  = (1 << b) - 1;
-        int32_t overflow = !!(val & (~mask));
-        return -(((-overflow) & mask) | (val & mask));
-    }
-}
 
 int
 get_number_of_cores() {
--- libovvc/ovutils.h
+++ libovvc/ovutils.h
@@ -27,11 +27,36 @@ ov_clip(int32_t val, int32_t a, int32_t b)
     return OVMIN(OVMAX(val, a), b);
 }
 
-uint32_t
-ov_clip_uintp2(int32_t val, uint32_t a);
+static inline uint32_t
+ov_clip_uintp2(int32_t val, uint32_t a)
+{
+    if (val > 0) {
+        int32_t mask  = (1 << a) - 1;
+        int32_t overflow = !!(val & (~mask));
+        return ((-overflow) & mask) | (val & mask);
+    } else {
+        return 0;
+    }
+    #if 0
+    return OVMIN(OVMAX(0, val), (1 << a) - 1);
+    #endif
+}
 
-int32_t
-ov_clip_intp2(int32_t val, uint32_t a);
+static inline int32_t
+ov_clip_intp2(int32_t val, uint32_t a)
+{
+    int b = a-1;
+    if (val > 0) {
+        int32_t mask  = (1 << b) - 1;
+        int32_t overflow = !!(val & (~mask));
+        return ((-overflow) & mask) | (val & mask);
+    } else {
+        val = -val;
+        int32_t mask  = (1 << b) - 1;
+        int32_t overflow = !!(val & (~mask));
+        return -(((-overflow) & mask) | (val & mask));
+    }
+}
 
 static inline int
 floor_log2(unsigned x)
