e755c6a4bdf4a013d8da2ed4ba94fd35b2f60c58
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-11-18 12:14:12 +0100

    Remove rcn_residual functions from rcn.c

--- libovvc/rcn.h
+++ libovvc/rcn.h
@@ -3,18 +3,6 @@
 
 #include "ovdefs.h"
 
-
-/* FIXME
- * rename OVINTRA
- * MERGE with INTER modes ?
- * STORE on uint8_t
- * find meaningful place for definition
- */
-
-
-#if 0
-extern const struct TrFunc tr_templates[NB_TR_TYPES][NB_TR_SIZES];
-#endif
 /* FIXME remove some args and give RCNCTX instead of ctudec
  */
 struct OVRCNCtx;
@@ -23,21 +11,6 @@ struct RCNFunctions;
 struct DBFInfo;
 struct OVBuffInfo;
 struct VVCGPM;
-
-void rcn_residual(OVCTUDec *const ctudec,
-             int16_t *const dst, int16_t *src,
-             uint8_t x0, uint8_t y0,
-             unsigned int log2_tb_w, unsigned int log2_tb_h,
-             unsigned int lim_cg_w,
-             uint8_t cu_mts_flag, uint8_t cu_mts_idx,
-             uint8_t is_dc, uint8_t lfnst_flag, uint8_t is_mip, uint8_t lfnst_idx, uint8_t sbt);
-
-void rcn_residual_c(OVCTUDec *const ctudec,
-                    int16_t *const dst, int16_t *src,
-                    uint8_t x0, uint8_t y0,
-                    uint8_t log2_tb_w, uint8_t log2_tb_h,
-                    uint16_t lim_cg_w,
-                    uint8_t lfnst_flag, uint8_t lfnst_idx);
 
 void rcn_frame_line_to_ctu(const struct OVRCNCtx *const rcn_ctx, uint8_t log2_ctb_s);
 
