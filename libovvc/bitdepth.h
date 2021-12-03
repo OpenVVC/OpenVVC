48e98926971d96f3dfbe7d852e669d0a1f51cf17
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2021-12-03 16:19:41 +0100

    Define bitdepth dependent OVSample type to uint8_t

--- libovvc/bitdepth.h
+++ libovvc/bitdepth.h
@@ -1,6 +1,12 @@
 #ifndef BITDEPTH_H
 #define BITDEPTH_H
 
+#if BITDEPTH > 8
+typedef uint8_t OVSample;
+#else
+typedef uint8_t OVSample;
+#endif
+
 #define ov_bdclip(a) ov_clip_uintp2(a, BITDEPTH)
 #define AVG_VAL ((1 << (BITDEPTH)) >> 1)
 
