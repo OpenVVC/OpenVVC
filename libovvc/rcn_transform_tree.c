4b022899fe86a2b0b7a1fc668dc9e2d4e0626077
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2022-02-10 10:31:52 +0100

    Clarify intra prediction log2_w determination in vertical ISP

--- libovvc/rcn_transform_tree.c
+++ libovvc/rcn_transform_tree.c
@@ -853,13 +853,16 @@ recon_isp_subtree_v(OVCTUDec *const ctudec,
          */
          /*FIXME separate small cases */
         if (!(offset_x & 0x3)) {
-            ctudec->rcn_funcs.intra_pred_isp(ctudec, &ctudec->rcn_ctx.ctu_buff.y[0],
-                               RCN_CTB_STRIDE, intra_mode, x0, y0,
-                               log2_pb_w >= 2 ? log2_pb_w : 2, log2_cb_h,
-                               log2_cb_w, log2_cb_h, offset_x, 0);
 
-            fill_ctb_bound(&ctudec->dbf_info, x0 & ~0x3, y0, log2_pb_w >= 2 ? log2_pb_w : 2, log2_cb_h);
-            fill_bs_map(&ctudec->dbf_info.bs2_map, x0 & ~0x3, y0, log2_pb_w >= 2 ? log2_pb_w : 2, log2_cb_h);
+            uint8_t log2_pred_w = log2_pb_w < 2 ? 2 : log2_pb_w;
+
+            ctudec->rcn_funcs.intra_pred_isp(ctudec, &ctudec->rcn_ctx.ctu_buff.y[0],
+                                             RCN_CTB_STRIDE, intra_mode, x0, y0,
+                                             log2_pred_w, log2_cb_h,
+                                             log2_cb_w, log2_cb_h, offset_x, 0);
+
+            fill_ctb_bound(&ctudec->dbf_info,      x0, y0, log2_pred_w, log2_cb_h);
+            fill_bs_map(&ctudec->dbf_info.bs2_map, x0, y0, log2_pred_w, log2_cb_h);
         }
 
         if (cbf) {
