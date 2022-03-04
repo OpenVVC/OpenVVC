ef8ec470a72e9ff5548eebfa0e4ef237ccba1252
Pierre-Loup Cabarat <pcabarat@insa-rennes.fr>
2022-03-04 17:32:19 +0100

    Use dynamic stride from CTU buffer structure in rcn_ctu* functions

--- libovvc/rcn_ctu.c
+++ libovvc/rcn_ctu.c
@@ -91,9 +91,10 @@ rcn_write_ctu_to_frame(const struct OVRCNCtx *const rcn_ctx, uint8_t log2_ctb_s)
 {
     int i;
     const struct OVBuffInfo *const fd = &rcn_ctx->frame_buff;
-    const OVSample *src_y  = &rcn_ctx->ctu_buff.y [0];
-    const OVSample *src_cb = &rcn_ctx->ctu_buff.cb[0];
-    const OVSample *src_cr = &rcn_ctx->ctu_buff.cr[0];
+    const struct OVBuffInfo *ctu_buff = &rcn_ctx->ctu_buff;
+    const OVSample *src_y  = &ctu_buff->y [0];
+    const OVSample *src_cb = &ctu_buff->cb[0];
+    const OVSample *src_cr = &ctu_buff->cr[0];
 
     OVSample *dst_y  = fd->y;
     OVSample *dst_cb = fd->cb;
@@ -101,15 +102,15 @@ rcn_write_ctu_to_frame(const struct OVRCNCtx *const rcn_ctx, uint8_t log2_ctb_s)
 
     for (i = 0; i < 1 << log2_ctb_s; i++) {
         memcpy(dst_y, src_y, sizeof(OVSample) << log2_ctb_s);
-        src_y += RCN_CTB_STRIDE;
+        src_y += ctu_buff->stride;
         dst_y += fd->stride;
     }
 
     for (i = 0; i < 1 << (log2_ctb_s - 1); ++i) {
         memcpy(dst_cb, src_cb, sizeof(OVSample) << (log2_ctb_s - 1));
         memcpy(dst_cr, src_cr, sizeof(OVSample) << (log2_ctb_s - 1));
-        src_cb += RCN_CTB_STRIDE;
-        src_cr += RCN_CTB_STRIDE;
+        src_cb += ctu_buff->stride_c;
+        src_cr += ctu_buff->stride_c;
         dst_cb += fd->stride_c;
         dst_cr += fd->stride_c;
     }
@@ -119,13 +120,14 @@ static void
 rcn_intra_line_to_ctu(const struct OVRCNCtx *const rcn_ctx, int x_l, uint8_t log2_ctb_s)
 {
     const struct OVBuffInfo *const il = &rcn_ctx->intra_line_buff;
+          struct OVBuffInfo *ctu_buff = &rcn_ctx->ctu_buff;
     const OVSample *src_y  = il->y  + x_l;
     const OVSample *src_cb = il->cb + (x_l>>1);
     const OVSample *src_cr = il->cr + (x_l>>1);
 
-    OVSample *dst_y  =  rcn_ctx->ctu_buff.y  - RCN_CTB_STRIDE;
-    OVSample *dst_cb =  rcn_ctx->ctu_buff.cb - RCN_CTB_STRIDE;
-    OVSample *dst_cr =  rcn_ctx->ctu_buff.cr - RCN_CTB_STRIDE;
+    OVSample *dst_y  =  ctu_buff->y  - ctu_buff->stride;
+    OVSample *dst_cb =  ctu_buff->cb - ctu_buff->stride_c;
+    OVSample *dst_cr =  ctu_buff->cr - ctu_buff->stride_c;
 
     memcpy(dst_y,  src_y , sizeof(OVSample) * ((1 << log2_ctb_s) + (1 << (log2_ctb_s - (log2_ctb_s == 7)))));
     memcpy(dst_cb, src_cb, sizeof(OVSample) * ((1 << (log2_ctb_s - 1)) + (1 << (log2_ctb_s - 1))));
@@ -136,19 +138,20 @@ static void
 rcn_ctu_to_intra_line(const struct OVRCNCtx *const rcn_ctx, int x_l, uint8_t log2_ctu_s)
 {
     const struct OVBuffInfo *intra_line_binfo = &rcn_ctx->intra_line_buff;
+          struct OVBuffInfo *ctu_buff = &rcn_ctx->ctu_buff;
 
     int max_cu_width_l = 1 << log2_ctu_s;
     int max_cu_width_c = 1 << (log2_ctu_s - 1);
 
     OVSample *_src;
 
-    _src = &rcn_ctx->ctu_buff.y[(max_cu_width_l - 1) * RCN_CTB_STRIDE];
+    _src = &ctu_buff->y[(max_cu_width_l - 1) * ctu_buff->stride];
     memcpy(&intra_line_binfo->y[x_l], _src, sizeof(*_src) << log2_ctu_s);
 
-    _src = &rcn_ctx->ctu_buff.cb[(max_cu_width_c - 1) * RCN_CTB_STRIDE ];
+    _src = &ctu_buff->cb[(max_cu_width_c - 1) * ctu_buff->stride_c];
     memcpy(&intra_line_binfo->cb[x_l>>1], _src, sizeof(*_src) << (log2_ctu_s - 1));
 
-    _src = &rcn_ctx->ctu_buff.cr[(max_cu_width_c - 1) * RCN_CTB_STRIDE ];
+    _src = &ctu_buff->cr[(max_cu_width_c - 1) * ctu_buff->stride_c];
     memcpy(&intra_line_binfo->cr[x_l>>1], _src, sizeof(*_src) << (log2_ctu_s - 1));
 }
 
@@ -157,9 +160,10 @@ rcn_write_ctu_to_frame_border(const struct OVRCNCtx *const rcn_ctx,
                               int last_ctu_w, int last_ctu_h)
 {
     const struct OVBuffInfo *const fd = &rcn_ctx->frame_buff;
-    const OVSample *src_y  = &rcn_ctx->ctu_buff.y [0];
-    const OVSample *src_cb = &rcn_ctx->ctu_buff.cb[0];
-    const OVSample *src_cr = &rcn_ctx->ctu_buff.cr[0];
+    const struct OVBuffInfo *ctu_buff = &rcn_ctx->ctu_buff;
+    const OVSample *src_y  = &ctu_buff->y [0];
+    const OVSample *src_cb = &ctu_buff->cb[0];
+    const OVSample *src_cr = &ctu_buff->cr[0];
 
     OVSample *dst_y  = fd->y;
     OVSample *dst_cb = fd->cb;
@@ -168,7 +172,7 @@ rcn_write_ctu_to_frame_border(const struct OVRCNCtx *const rcn_ctx,
     for (int i = 0; i < last_ctu_h; ++i) {
         memcpy(dst_y, src_y, sizeof(*src_y) * last_ctu_w);
         dst_y += fd->stride;
-        src_y += RCN_CTB_STRIDE;
+        src_y += ctu_buff->stride;
     }
 
     for (int i = 0; i < (last_ctu_h >> 1); i++) {
@@ -176,8 +180,8 @@ rcn_write_ctu_to_frame_border(const struct OVRCNCtx *const rcn_ctx,
         memcpy(dst_cr, src_cr,  sizeof(*src_cr) * (last_ctu_w >> 1));
         dst_cb += fd->stride_c;
         dst_cr += fd->stride_c;
-        src_cb += RCN_CTB_STRIDE;
-        src_cr += RCN_CTB_STRIDE;
+        src_cb += ctu_buff->stride_c;
+        src_cr += ctu_buff->stride_c;
     }
 }
 
