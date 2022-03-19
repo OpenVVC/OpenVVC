54b1fde0b3e51a43b5c694c3a1158ff3bb49429e
Guillaume GAUTIER <gugautie@insa-rennes.fr>
2022-03-19 16:28:12 +0100

    ARM: PLANAR PDPC assembly optimisations

--- libovvc/arm/rcn_intra_dc_planar_neon_wrapper.c
+++ libovvc/arm/rcn_intra_dc_planar_neon_wrapper.c
@@ -52,6 +52,11 @@
                                                                          uint16_t *const dst, ptrdiff_t dst_stride,     \
                                                                          int log2_pb_w, int log2_pb_h);
 
+#define DECALARE_PLANAR_PDPC(w, h) void ov_planar_pdpc_w ## w ## _h ## h ##_neon(const uint16_t *const src_above,               \
+                                                                                 const uint16_t *const src_left,                \
+                                                                                 uint16_t *const dst, ptrdiff_t dst_stride,     \
+                                                                                 int log2_pb_w, int log2_pb_h);                                                                         
+
 DECALARE_DC_PDPC( 4,  4)
 DECALARE_DC_PDPC( 4,  8)
 DECALARE_DC_PDPC( 4, 16)
@@ -81,6 +86,36 @@ DECALARE_DC_PDPC(64,  8)
 DECALARE_DC_PDPC(64, 16)
 DECALARE_DC_PDPC(64, 32)
 DECALARE_DC_PDPC(64, 64)
+
+DECALARE_PLANAR_PDPC( 4,  4)
+DECALARE_PLANAR_PDPC( 4,  8)
+DECALARE_PLANAR_PDPC( 4, 16)
+DECALARE_PLANAR_PDPC( 4, 32)
+DECALARE_PLANAR_PDPC( 4, 64)
+
+DECALARE_PLANAR_PDPC( 8,  4)
+DECALARE_PLANAR_PDPC( 8,  8)
+DECALARE_PLANAR_PDPC( 8, 16)
+DECALARE_PLANAR_PDPC( 8, 32)
+DECALARE_PLANAR_PDPC( 8, 64)
+
+DECALARE_PLANAR_PDPC(16,  4)
+DECALARE_PLANAR_PDPC(16,  8)
+DECALARE_PLANAR_PDPC(16, 16)
+DECALARE_PLANAR_PDPC(16, 32)
+DECALARE_PLANAR_PDPC(16, 64)
+
+DECALARE_PLANAR_PDPC(32,  4)
+DECALARE_PLANAR_PDPC(32,  8)
+DECALARE_PLANAR_PDPC(32, 16)
+DECALARE_PLANAR_PDPC(32, 32)
+DECALARE_PLANAR_PDPC(32, 64)
+
+DECALARE_PLANAR_PDPC(64,  4)
+DECALARE_PLANAR_PDPC(64,  8)
+DECALARE_PLANAR_PDPC(64, 16)
+DECALARE_PLANAR_PDPC(64, 32)
+DECALARE_PLANAR_PDPC(64, 64)
 
 void rcn_init_dc_planar_functions_neon(struct RCNFunctions *const rcn_funcs){
     //DC
@@ -113,4 +148,35 @@ void rcn_init_dc_planar_functions_neon(struct RCNFunctions *const rcn_funcs){
     rcn_funcs->dc.pdpc[4][2] = &ov_dc_pdpc_w64_h16_neon;
     rcn_funcs->dc.pdpc[4][3] = &ov_dc_pdpc_w64_h32_neon;
     rcn_funcs->dc.pdpc[4][4] = &ov_dc_pdpc_w64_h64_neon;
+
+    //Planar
+    rcn_funcs->planar.pdpc[0][0] = &ov_planar_pdpc_w4_h4_neon;
+    rcn_funcs->planar.pdpc[0][1] = &ov_planar_pdpc_w4_h8_neon;
+    rcn_funcs->planar.pdpc[0][2] = &ov_planar_pdpc_w4_h16_neon;
+    rcn_funcs->planar.pdpc[0][3] = &ov_planar_pdpc_w4_h32_neon;
+    rcn_funcs->planar.pdpc[0][4] = &ov_planar_pdpc_w4_h64_neon;
+
+    rcn_funcs->planar.pdpc[1][0] = &ov_planar_pdpc_w8_h4_neon;
+    rcn_funcs->planar.pdpc[1][1] = &ov_planar_pdpc_w8_h8_neon;
+    rcn_funcs->planar.pdpc[1][2] = &ov_planar_pdpc_w8_h16_neon;
+    rcn_funcs->planar.pdpc[1][3] = &ov_planar_pdpc_w8_h32_neon;
+    rcn_funcs->planar.pdpc[1][4] = &ov_planar_pdpc_w8_h64_neon;
+
+    rcn_funcs->planar.pdpc[2][0] = &ov_planar_pdpc_w16_h4_neon;
+    rcn_funcs->planar.pdpc[2][1] = &ov_planar_pdpc_w16_h8_neon;
+    rcn_funcs->planar.pdpc[2][2] = &ov_planar_pdpc_w16_h16_neon;
+    rcn_funcs->planar.pdpc[2][3] = &ov_planar_pdpc_w16_h32_neon;
+    rcn_funcs->planar.pdpc[2][4] = &ov_planar_pdpc_w16_h64_neon;
+
+    rcn_funcs->planar.pdpc[3][0] = &ov_planar_pdpc_w32_h4_neon;
+    rcn_funcs->planar.pdpc[3][1] = &ov_planar_pdpc_w32_h8_neon;
+    rcn_funcs->planar.pdpc[3][2] = &ov_planar_pdpc_w32_h16_neon;
+    rcn_funcs->planar.pdpc[3][3] = &ov_planar_pdpc_w32_h32_neon;
+    rcn_funcs->planar.pdpc[3][4] = &ov_planar_pdpc_w32_h64_neon;
+
+    rcn_funcs->planar.pdpc[4][0] = &ov_planar_pdpc_w64_h4_neon;
+    rcn_funcs->planar.pdpc[4][1] = &ov_planar_pdpc_w64_h8_neon;
+    rcn_funcs->planar.pdpc[4][2] = &ov_planar_pdpc_w64_h16_neon;
+    rcn_funcs->planar.pdpc[4][3] = &ov_planar_pdpc_w64_h32_neon;
+    rcn_funcs->planar.pdpc[4][4] = &ov_planar_pdpc_w64_h64_neon;
 }
